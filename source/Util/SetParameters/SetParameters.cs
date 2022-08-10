5ca88a5e19a429424ae63159c1e4b0807f8d76a3
党帅|shuai dang <109052398+shuaidang@users.noreply.github.com>
2022-08-10 16:53:45 +0800

    Update SetParameters.cs

--- source/Util/SetParameters/SetParameters.cs
+++ source/Util/SetParameters/SetParameters.cs
@@ -51,7 +51,7 @@ class sample
             Console.WriteLine("Input invalid! Please enter the device index you wnat to connect: ");
         }
 
-        //MechEyeDeviceInfo deviceInfo = new MechEyeDeviceInfo() { model = "", id = "", hardwareVersion = "", firmwareVersion = "1.5.0", ipAddress = "127.0.0.1", port = 5577 };
+        //MechEyeDeviceInfo deviceInfo = new MechEyeDeviceInfo() { model = "", id = "", hardwareVersion = "", firmwareVersion = "1.6.0", ipAddress = "127.0.0.1", port = 5577 };
 
         ErrorStatus status = new ErrorStatus();
         MechEyeDevice device = new MechEyeDevice();
